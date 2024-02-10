77b346bf60e0d894f555bfcfe1629de021fde6a0
BIBIZANA <inikband@gmail.com>
2024-02-10 15:00:54 +0300

    i dont know

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from turtle import *
-hideturtle()
-speed(10)
 t1= Turtle()
 t2= Turtle()
 t3=Turtle()
@@ -8,10 +6,9 @@ t3=Turtle()
 t1.color('red')
 t2.color('green')
 t3.color('blue')
-t2.left(162)
+t2.left(120)
 t3.left(270)
-
-for i in range(28):
+for i in range(37):
     t1.forward(10)
     t1.left(10)
 
@@ -20,6 +17,6 @@ for i in range(28):
 
     t3.forward(10)
     t3.left(10)
-
+hideturtle()
 exitonclick()
 
